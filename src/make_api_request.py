3356f5cfde9349e64fb9d3c4f301d1f59995ef8d
Kahlail <k40075@outlook.com>
2023-06-28 19:36:51 +0100

    complete api_get_request and happy path unit test

--- src/make_api_request.py
+++ src/make_api_request.py
@@ -1,6 +1,9 @@
 def api_get_request(req_lib, url: str, params: list):
-    response = req_lib.get(url, params)
-    response = response.json()
-    status = response.status_code
-    return response, status
+    try:
+        response = req_lib.get(url, params)
+        if response.status_code == req_lib.codes.ok:
+            return response.json(), response.status_code
+    except Exception as e:
+            return f'Request failed!: {e}', response.status_code
+    return None, None
 
--- tests/test_api_request.py
+++ tests/test_api_request.py
@@ -5,18 +5,23 @@ def test_api_request_good_response():
     mock_url = 'https://www.mock-url.com/get'
     mock_parameters = ['value 1', 'value 2']
     mock_request_lib = Mock()
-    mock_request_lib.get.return_value = Mock()
-    mock_request_lib.get.return_value.status_value.return_value = 200
-    mock_request_lib.get.return_value.json.return_value = {
-      "id": 1,
-      "name": "Mock Sanchez",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
+    
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = [{
+        "id": 1,
+        "name": "Mock Sanchez",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Male",
     }
+]
+    
+    mock_request_lib.get.return_value = mock_response
+    mock_request_lib.codes.ok = 200
 
-
+    expected_status_code = 200
     expected_response =[
     {
       "id": 1,
@@ -27,10 +32,8 @@ def test_api_request_good_response():
       "gender": "Male",
     }
 ]
-    expected_status_code = 200
-    
-    true_response, true_stat_code = api_get_request(mock_request_lib, mock_url, mock_parameters)
-    
-    
-    assert expected_response == true_response
-    assert expected_status_code == true_stat_code
+
+    actual_response, actual_stat_code = api_get_request(mock_request_lib, mock_url, mock_parameters)
+
+    assert expected_response == actual_response
+    assert expected_status_code == actual_stat_code
